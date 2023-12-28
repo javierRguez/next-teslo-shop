64e683758d4bab25194580a1b7495a9f6a778755
JavierRodriguez <jotarodriguez939@gmail.com>
2023-12-28 20:38:30 +0100

    add component title

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,9 @@
-import { titleFont } from "@/config/fonts";
+import { Title } from "@/components";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Hola Mundo</h1>
-      <h1 className={`${titleFont.className} font-bold`}>Hola Mundo</h1>
-    </div>
+    <>
+      <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
+    </>
   );
 }
