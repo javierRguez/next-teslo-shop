07aff33ca9d7c8ab388a965ac4e077a455448541
JavierRodriguez <jotarodriguez939@gmail.com>
2023-12-28 19:36:56 +0100

    creando rutas de la aplicacion

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,9 +2,9 @@ import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <main className="">
+    <div className="">
       <h1>Hola Mundo</h1>
       <h1 className={`${titleFont.className} font-bold`}>Hola Mundo</h1>
-    </main>
+    </div>
   );
 }
