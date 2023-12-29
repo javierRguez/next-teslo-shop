3b798f06fe7a92e748fc153278afa1418dbf422f
JavierRodriguez <jotarodriguez939@gmail.com>
2023-12-29 01:35:56 +0100

    add first interfaces & productGrid component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { TopMenu } from "./ui/top-menu/TopMenu";
 export { PageNotFound } from "./ui/not-found/PageNotFound";
 export { Title } from "./ui/title/Title";
+export { ProductGrid } from "./products/product-grid/ProductGrid";
