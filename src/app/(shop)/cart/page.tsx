869ebc45f856950415a51d36113a89eff372a71b
JavierRodriguez <jotarodriguez939@gmail.com>
2024-01-10 00:41:49 +0100

    finalizando diseño general

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,7 +1,66 @@
+import { QuantitySelector, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function CartPage() {
+  const productInCart = [
+    initialData.products[0],
+    initialData.products[1],
+    initialData.products[2],
+  ];
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Carrito" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Agregar más items</span>
+            <Link href="/" className="underline mb-5">
+              Continuar comprando
+            </Link>
+
+            {productInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  alt={product.title}
+                  className="mr-5 rounded w-28 h-28"
+                />
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price}</p>
+                  <QuantitySelector quantity={3} />
+                  <button className="underline mt-3">Remover</button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="bg-white rounded-xl shadow-xl p-7 h-fit">
+            <h2 className="text-2xl mb-2">Resumen de orden</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Productos</span>
+              <span className="text-right">3 artículos</span>
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$ 100</span>
+              <span className="text-2xl mt-5">Total:</span>
+              <span className="text-2xl mt-5 text-right">$ 100</span>
+            </div>
+            <div className="w-full mt-5">
+              <Link
+                className="flex btn-primary justify-center"
+                href="/checkout/address"
+              >
+                Checkout
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,7 +1,89 @@
+import { Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function CheckoutPage() {
+  const productInCart = [
+    initialData.products[0],
+    initialData.products[1],
+    initialData.products[2],
+  ];
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Verificar orden" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Ajustar elementos</span>
+            <Link href="/" className="underline mb-5">
+              Editar carrito
+            </Link>
+
+            {productInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  alt={product.title}
+                  className="mr-5 rounded w-28 h-28"
+                />
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price} x 3</p>
+                  <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                  <button className="underline mt-3">Remover</button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2 font-bold">Dirección de entrega</h2>
+            <div className="mb-10">
+              <p className="text-xl">Fernando Herrera</p>
+              <p>Av. Siempre</p>
+              <p>Col. Centro</p>
+              <p>Alcaldia</p>
+              <p>Ciudad de Mexico</p>
+              <p>CP 123123</p>
+              <p>123123132</p>
+            </div>
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
+            <h2 className="text-2xl mb-2 font-bold">Resumen de orden</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Productos</span>
+              <span className="text-right">3 artículos</span>
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$ 100</span>
+              <span className="text-2xl mt-5">Total:</span>
+              <span className="text-2xl mt-5 text-right">$ 100</span>
+            </div>
+            <div className="w-full mb-5 mt-5">
+              <p className="mb-5">
+                <span className="text-xs">
+                  Al hacer click en &quot;Colocar orden&quot;, acepta nuestros{" "}
+                  <a href="#" className="underline">
+                    terminos y condiciones
+                  </a>{" "}
+                  y{" "}
+                  <a href="#" className="underline">
+                    política de privacidad
+                  </a>
+                </span>
+              </p>
+              <Link
+                className="flex btn-primary justify-center"
+                href="/orders/123"
+              >
+                Colocar orden
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
