8bdb5c0080bab6283d2e269a0ac2a4e4b30e0aae
JavierRodriguez <jotarodriguez939@gmail.com>
2024-01-11 14:21:26 +0100

    first server action & init pagination products

---
+++ src/actions/products/product-pagination.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+interface PaginationOptions {
+  page?: number;
+  take?: number;
+}
+
+export const getPaginatedProductsWithImages = async ({
+  page = 1,
+  take = 12,
+}: PaginationOptions) => {
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
+  if (isNaN(Number(take))) take = 12;
+  if (take < 1) take = 1;
+  try {
+    const products = await prisma.product.findMany({
+      take: take,
+      skip: (page - 1) * take,
+      include: {
+        ProductImage: {
+          take: 2,
+          select: {
+            url: true,
+          },
+        },
+      },
+    });
+    return {
+      currentPage: 1,
+      totalPages: 10,
+      products: products.map((product) => ({
+        ...product,
+        images: product.ProductImage.map((image) => image.url),
+      })),
+    };
+  } catch (error) {
+    throw new Error("No se pudo recuperar los productos");
+  }
+};
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,15 @@
+import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
 
-const products = initialData.products;
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
 
-export default function Home() {
+export default async function Home({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+  const { products } = await getPaginatedProductsWithImages({ page });
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
