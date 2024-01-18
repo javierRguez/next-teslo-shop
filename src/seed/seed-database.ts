57e6b71d7ee1736cd46cd168ca666459d0e79a30
JavierRodriguez <jotarodriguez939@gmail.com>
2024-01-18 14:04:38 +0100

    add prisma schema & update seed

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -4,13 +4,16 @@ import prisma from "../lib/prisma";
 async function main() {
   // 1. Borrar registros previos
   //await Promise.all([
+  await prisma.user.deleteMany();
   await prisma.productImage.deleteMany();
   await prisma.product.deleteMany();
   await prisma.category.deleteMany();
   //]);
 
   // Categorias
-  const { categories, products } = initialData;
+  const { categories, products, users } = initialData;
+
+  await prisma.user.createMany({ data: users });
 
   const categoriesData = categories.map((category) => ({
     name: category,
