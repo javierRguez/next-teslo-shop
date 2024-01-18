8cba71cc1096697b170f91f63e438f7ab10ac1fe
JavierRodriguez <jotarodriguez939@gmail.com>
2024-01-18 12:09:40 +0100

    WIP add auth

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -53,11 +53,14 @@ export const TopMenu = () => {
         <Link href="/search" className="mx-2">
           <IoSearchOutline className="w-5 h-5" />
         </Link>
-        <Link href="/cart" className="mx-2">
+        <Link
+          href={totalItemsInCart === 0 && loaded ? "/empty" : "/cart"}
+          className="mx-2"
+        >
           <div className="relative">
             {loaded && totalItemsInCart > 0 && (
               <span
-                className="absolute animate-bounce text-xs px-1
+                className=" absolute animate-bounce text-xs px-1
             rounded-full  font-bold -top-2
              -right-2 bg-blue-400 text-white"
               >
